8610018329c3e5efaf5325fe2d27559d9b69df02
Dafne <lire.alarcon.06@gmail.com>
2026-03-05 01:16:52 -0600

    Middleware imprime miuestra error

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,7 +1,7 @@
 function errorHandler(err, req, res, next) {
   console.log('Error:', err.message);
 
-  return res.status(500).json({error: 'Internal Server Error'})
+  return res.status(500).json({error: err.message, text:'Internal Server Error', })
 }
 
 module.exports = { errorHandler }
